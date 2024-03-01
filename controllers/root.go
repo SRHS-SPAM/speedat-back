a04eb041f26554a95223ef8a8e33f9e54f88a56b
정삼복 <68502584+3boku@users.noreply.github.com>
2024-03-01 18:50:07 +0900

    Update root.go

--- controllers/root.go
+++ controllers/root.go
@@ -13,7 +13,7 @@ func NewController(port string) {
 
 	r.Use(gin.Logger())
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"https://www.speedat.site/", "http://112.159.30.237", "http://122.203.181.62", "http://175.114.18.77", "http://180.70.171.163", "http://210.204.194.10", "http://58.224.240.125"},
+		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
 		MaxAge:       24 * time.Hour,
 	}))
